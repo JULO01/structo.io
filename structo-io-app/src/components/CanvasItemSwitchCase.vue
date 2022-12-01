e4f19034c24455cbed07d776883772961a4d3c9b
Julian Götze <90795588+JULO01@users.noreply.github.com>
2022-12-01 07:50:52 +0100

    feat(ui): adding SwitchCase

--- structo-io-app/src/components/CanvasItemSwitchCase.vue
+++ structo-io-app/src/components/CanvasItemSwitchCase.vue
@@ -1,3 +1,108 @@
-<template><div class="item-container"></div></template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<template>
+  <div class="item-container">
+    <div class="item-argument-left-triangle"></div>
+    <div class="item-argument-right-triangle">
+      <!-- <div class="test-argument"></div>-->
+    </div>
+    <div class="item-childzone-container">
+      <!--<div v-for="item in testItems" :key="item" class="item-childzone-case">
+        <div class="item-childzone-case-argument"></div>
+        <div class="item-childzone-case-child"></div>
+      </div> 
+      <div class="item-childzone-else">
+        <div class="item-childzone-else-child"></div>
+      </div> -->
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const testItems = ref([1, 2, 3, 4])
+</script>
+<style scoped>
+/*.item-container {
+  border-radius: 10px;
+  min-height: var(--min-height-editor-item);
+  min-width: var(--min-width-editor-item);
+  height: fit-content;
+  width: fit-content;
+
+  display: flex;
+  flex-direction: column;
+}*/
+.item-container {
+  min-height: var(--min-height-editor-item);
+  min-width: var(--min-width-editor-item);
+}
+.item-argument-container {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  min-height: 2rem;
+}
+.item-argument-left-triangle {
+  background-image: url('data:image/svg+xml;charset=UTF-8, <svg preserveAspectRatio="none" viewBox="0 0 331 102" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="332" y2="101.146" stroke="rgba(256, 256, 256, 0.9)" stroke-width="2"/></svg>');
+  background-size: 100% 100%;
+  display: flex;
+}
+.test-argument {
+  min-width: 3rem;
+}
+.item-argument-right-triangle {
+  background-image: url('data:image/svg+xml;charset=UTF-8, <svg preserveAspectRatio="none" viewBox="0 0 332 104" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="331.299" y1="0" x2="0" y2="103.146" stroke="rgb(35, 35, 35)" stroke-width="2"/></svg>');
+  background-size: 100% 100%;
+  display: flex;
+}
+
+html:not(.dark) .item-container {
+  border: 2px solid var(--color-text-active-light);
+}
+html:not(.dark) .item-childzone {
+  border: 2px solid var(--color-text-active-light);
+}
+html:not(.dark) .item-childzone:hover {
+  border: 2px solid var(--color-primary);
+}
+html:not(.dark) .item-argument-container {
+  border: 2px solid var(--color-text-active-light);
+}
+html.dark .item-container {
+  border: 2px solid var(--color-text-active-dark);
+}
+html.dark .item-childzone {
+  border: 2px solid var(--color-text-active-dark);
+}
+html.dark .item-argument-container {
+  border: 2px solid var(--color-text-active-dark);
+}
+html.dark .item-childzone:hover {
+  border: 2px solid var(--color-primary);
+}
+.item-container {
+  display: grid;
+  grid-template-columns: auto, 10rem;
+  grid-template-rows: auto;
+}
+.item-argument-left-triangle {
+  grid-column: 1;
+  grid-row: 1;
+  width: 100%;
+  justify-self: start;
+  background-color: brown;
+}
+.item-argument-right-triangle {
+  width: 100%;
+  grid-column: 2;
+  grid-row: 1;
+  background-color: red;
+  justify-self: end;
+}
+.item-childzone-container {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row: 2;
+  width: 200%;
+  background-color: green;
+}
+</style>
