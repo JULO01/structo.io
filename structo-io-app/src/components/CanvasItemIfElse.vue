637e8f1b83eb2ef029bfce82232fb333d4d6826d
Julian Götze <90795588+JULO01@users.noreply.github.com>
2022-12-04 11:51:14 +0100

    feat: general designing method and statement elements

--- structo-io-app/src/components/CanvasItemIfElse.vue
+++ structo-io-app/src/components/CanvasItemIfElse.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="item-container">
-    <div class="item-argument-container">
-      <div class="item-argument-if-display"></div>
-      <div class="item-argument"></div>
-      <div class="item-argument-else-display"></div>
+    <div class="item-argument-container triangle-background" data-colored="true">
+      <div class="item-argument-if-display">Hallo</div>
+      <div class="item-argument">Moin</div>
+      <div class="item-argument-else-display">Else</div>
     </div>
     <div class="item-childzone-container">
       <div class="item-childzone-if"></div>
@@ -12,68 +12,36 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
-import IfRectangle from '../assets/svg/shapes/IfRectangle.vue'
-</script>
+<script setup lang="ts"></script>
 <style scoped>
-/* Creating a polygon :
-clip-path: polygon(0% 0%,
-                   100% 0%,
-                   50% 50%,
-                    50% 50%);
-                    */
-html:not(.dark) .item-container {
-  border: 2px solid var(--color-text-active-light);
-}
-html:not(.dark) .item-childzone {
-  border: 2px solid var(--color-text-active-light);
-}
-html:not(.dark) .item-childzone:hover {
-  border: 2px solid var(--color-primary);
-}
-html:not(.dark) .item-argument-container {
-  border: 2px solid var(--color-text-active-light);
-}
-html.dark .item-container {
-  border: 2px solid var(--color-text-active-dark);
-}
-html.dark .item-childzone {
-  border: 2px solid var(--color-text-active-dark);
-}
-html.dark .item-argument-container {
-  border: 2px solid var(--color-text-active-dark);
-}
-html.dark .item-childzone:hover {
-  border: 2px solid var(--color-primary);
-}
 .item-argument-container {
   height: fit-content;
-  min-height: var(--min-height-editor-argument);
   position: relative;
-
   display: flex;
   flex-direction: row;
   border-top: none !important;
   border-left: none !important;
   border-right: none !important;
 }
-html:not(.dark) .item-argument-container {
-  background-image: url('data:image/svg+xml;charset=UTF-8, <svg preserveAspectRatio="none" viewBox="0 0 331 102" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="0.519784" y1="0.145702" x2="166.52" y2="101.146" stroke="rgb(53, 53, 53)" stroke-width="2"/><line x1="329.523" y1="0.852376" x2="166.523" y2="100.852" stroke="rgb(53, 53, 53)" stroke-width="2"/></svg>');
-  background-size: 100% 100%;
-}
-html.dark .item-argument-container {
-  background-image: url('data:image/svg+xml;charset=UTF-8, <svg preserveAspectRatio="none" viewBox="0 0 331 102" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="0.519784" y1="0.145702" x2="166.52" y2="101.146" stroke="rgba(256, 256, 256, 0.9)" stroke-width="2"/><line x1="329.523" y1="0.852376" x2="166.523" y2="100.852" stroke="rgba(256, 256, 256, 0.9)" stroke-width="2"/></svg>');
-  background-size: 100% 100%;
-}
 .item-argument {
   min-height: 2rem;
+  width: 33%;
+  background-color: red;
   height: fit-content;
+  display: flex;
+  align-content: flex-end;
+  justify-content: center;
 }
 .item-argument-if-display {
-  width: 60%;
+  text-align: center;
+  width: 33%;
+  display: flex;
+  align-content: flex-end;
+  justify-content: flex-start;
 }
 .item-argument-else-display {
-  width: 40%;
+  width: 33%;
+  text-align: center;
 }
 .item-container {
   border-radius: 10px;
--- structo-io-app/src/components/CanvasItemMethodBlock.vue
+++ structo-io-app/src/components/CanvasItemMethodBlock.vue
@@ -1,3 +1,41 @@
-<template><div></div></template>
+<template>
+  <div class="method-item-container">
+    <div class="item-statement"></div>
+    <div class="item-childzone"></div>
+  </div>
+</template>
 <script setup lang="ts"></script>
-<style scoped></style>
+<style scoped>
+.method-item-container {
+  min-width: initial;
+  height: fit-content;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  padding-left: 1.5rem;
+}
+.item-statement {
+  position: relative;
+  border-radius: 10px;
+  border: 2px solid black;
+  min-width: var(--min-width-editor-item);
+  min-height: var(--min-height-editor-argument);
+}
+.item-statement::before {
+  content: '';
+  position: absolute;
+  left: 10%;
+  border-left: 2px solid black;
+  height: 100%;
+}
+.item-statement::after {
+  content: '';
+  position: absolute;
+  left: 90%;
+  border-left: 2px solid black;
+  height: 100%;
+}
+.item-childzone {
+  border: none;
+}
+</style>
