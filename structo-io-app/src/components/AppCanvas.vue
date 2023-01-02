d254386928c53df2e5345d918daaf4393166c65f
JULO01 <90795588+JULO01@users.noreply.github.com>
2023-01-02 16:32:26 +0100

    feat(ui): optimizing dark-mode css

--- structo-io-app/src/components/AppCanvas.vue
+++ structo-io-app/src/components/AppCanvas.vue
@@ -8,28 +8,80 @@
       <CanvasSidebarItem :type="'method-block'" />
       <CanvasSidebarItem :type="'switch-case'" />
     </div>
-    <div class="canvas">
-      <BaseEmptyContent v-if="false" url="src/assets/svg/drag.svg" display-text="Drag a node inside to get started!" alt="TODO: entfern mich" />
+    <div class="canvas" :class="{ 'childzone-shown': showDropHighlights }" @drop.prevent="handleDrop" @dragenter.prevent="handleDragEnter" @dragleave="handleDragLeave" @dragover.prevent>
+      <component v-if="rootElementType != null" :is="rootElement" />
+      <BaseEmptyContent v-if="rootElementType == null && !store.$state.activeDragging" url="src/assets/svg/drag.svg" display-text="Drag a node inside to get started!" alt="TODO: entfern mich" />
+      <!--<CanvasItemIfElse /> -->
+      <!--Todo: wrapper for all elements-->
       <!-- <CanvasItemSwitchCase /> -->
       <!--<CanvasItemStatement />-->
       <!--<CanvasItemMethodBlock />-->
       <!--<CanvasItemDoWhile /> cleaned up -->
       <!--<CanvasItemWhileDo /> cleaned up -->
-      <CanvasItemIfElse />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import BaseEmptyContent from './BaseEmptyContent.vue'
 import CanvasItemDoWhile from './CanvasItemDoWhile.vue'
+import BaseTextInput from './BaseTextInput.vue'
 import CanvasItemWhileDo from './CanvasItemWhileDo.vue'
 import CanvasItemSwitchCase from './CanvasItemSwitchCase.vue'
 import CanvasItemIfElse from './CanvasItemIfElse.vue'
 import CanvasSidebarItem from './CanvasSidebarItem.vue'
 import CanvasItemStatement from './CanvasItemStatement.vue'
 import CanvasItemMethodBlock from './CanvasItemMethodBlock.vue'
+import { ref, computed } from 'vue'
+import { useGlobalStore } from '../stores/globalStore'
+import { Node, NodeType } from '../types/nodes'
+
+const store = useGlobalStore()
+
+const canvasItems = {
+  'do-while': CanvasItemDoWhile,
+  'method-block': CanvasItemMethodBlock,
+  statement: CanvasItemStatement,
+  'switch-case': CanvasItemSwitchCase,
+  'while-do': CanvasItemWhileDo,
+  'if-else': CanvasItemIfElse,
+}
+
+const rootElementType = ref<NodeType | null>(null)
+const rootElement = computed(() => {
+  if (!rootElementType.value) return null
+  return canvasItems[rootElementType.value]
+})
+
+const showDropHighlights = computed(() => {
+  return rootElementType.value == null && store.$state.activeDragging
+})
+
+function handleDragEnter(event: DragEvent) {
+  //TODO: highlight canvas background
+  if (!showDropHighlights.value) return
+  const target = event.target
+  target.classList.add('childzone-dragover')
+  return
+}
+
+function handleDragLeave(event: DragEvent) {
+  if (!showDropHighlights.value) return
+  const target = event.target
+  target.classList.remove('childzone-dragover')
+  //TODO: highlight canvas background
+  return
+}
+
+function handleDrop(event: DragEvent) {
+  if (rootElementType.value != null) return
+  const childType = event.dataTransfer?.getData('text/plain') as NodeType
+  rootElementType.value = childType
+}
 </script>
 <style scoped>
+.canvas {
+  background-color: var(--editor-canvas-background);
+}
 .canvas-container {
   --padding-horizontal: clamp(1.5rem, 3vw, 2.5rem);
   --padding-horizontal-test: 1.5vw;
@@ -59,5 +111,12 @@ import CanvasItemMethodBlock from './CanvasItemMethodBlock.vue'
   position: absolute;
   top: 0;
   bottom: 0;
+}
+.childzone-shown {
+  background-color: rgba(var(--color-primary-500), 0.1);
+}
+
+.childzone-dragover {
+  background-color: rgba(var(--color-primary-500), 0.3);
 }
 </style>
